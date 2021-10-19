6df5259d69b1ece28a5b4d6e07eb5587789b4cc6
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-19 16:50:46 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,3 +1,14 @@
-export class AccountMongoRepository {
-  
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/Account';
+import { AddAccountModel } from '../../../../domain/usecases/add-account';
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne({ ...accountData })
+    const accountMongo = result.ops[0]
+    const { _id, ...accountWithoudId } = accountMongo
+    return { ...accountWithoudId, id: _id }
+  }
 }
