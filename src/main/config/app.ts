7aed490f41ecd525596902bf3a0ed625f202561e
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-19 18:58:59 -0300

    feat: ensure SignUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import routes from './routes'
 
 const app = express()
 setupMiddlewares(app)
+routes(app)
 export default app
