2efccbb027a1529c1538d5906e027e30112c70e5
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-19 12:02:28 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,9 @@
 import { EmailValidator } from "../presentation/protocols/email-validator";
+import validator from 'validator'
 
 export default class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
-    return false
+    const isEmail = validator.isEmail(email)
+    return isEmail
   }
 }
