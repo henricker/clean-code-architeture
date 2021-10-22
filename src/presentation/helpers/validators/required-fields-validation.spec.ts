8da6d0259ba969f81837d4507a0f48a074744213
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-22 15:44:39 -0300

    test: ensure RequiredFieldsValidation return MissinParamError if fieldName not found on input

---
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -0,0 +1,23 @@
+import { MissingParamError } from "../../errors"
+import { RequiredFieldsValidation } from "./required-fields-validation"
+
+interface ISutType {
+  sut: RequiredFieldsValidation
+}
+
+
+const makeSut = (): ISutType => {
+  const sut = new RequiredFieldsValidation('fieldName')
+  return {
+    sut
+  }
+}
+
+describe('RequiredFieldsValidation', () => {
+  it('should return MissingParamError if fieldName not found on input', () => {
+    const { sut } = makeSut()
+    const mockInput = { field: 'fieldValue' }
+    const error = sut.validate(mockInput)
+    expect(error).toEqual(new MissingParamError('fieldName'))
+  })
+})
