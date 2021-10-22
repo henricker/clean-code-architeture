4ccf42a0d5722928b93201c510b88fe8082cb70d
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-22 13:30:41 -0300

    test: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,7 @@
 import { HttpRequest, HttpResponse } from "../../protocols"
 import { LoginController } from "./login"
-import { badRequest } from '../../helpers/http-helper'
-import { InvalidParamError, MissingParamError } from "../../errors"
+import { badRequest, serverError } from '../../helpers/http-helper'
+import { InvalidParamError, MissingParamError, ServerError } from "../../errors"
 import { EmailValidator } from "../signup/signup-protocols"
 
 interface ISutType {
@@ -85,5 +85,18 @@ describe('Login Controller', () => {
     await sut.handle(makeFakeRequest())
 
     expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
+  })
+
+  it('should return 500 if EmailValidator throws', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const error = new Error()
+    error.stack = 'any_error'
+
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce((email) => {
+      throw error
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(error))
   })
 })
