a1a1b6ddef6a8a857dfd936030929670a66856a6
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-22 13:23:45 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -8,15 +8,15 @@ export class LoginController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if(!httpRequest.body?.email) {
+    const { email, password } = httpRequest.body
+
+    if(!email) {
       return badRequest(new MissingParamError('email'))
     }
 
-    if(!httpRequest.body?.password) {
+    if(!password) {
       return badRequest(new MissingParamError('password'))
     }
-
-    const { email, password } = httpRequest.body
 
     const isValid = this.emailValidator.isValid(email)
     
