ce7ac8492eb7b567d017b81731465b19417298cc
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-22 15:44:28 -0300

    feat: ensure RequiredFieldsValidation return MissinParamError if fieldName not found on input

---
+++ src/presentation/helpers/validators/required-fields-validation.ts
@@ -0,0 +1,12 @@
+import { MissingParamError } from "../../errors";
+import { Validation } from "../../protocols/validation";
+
+export class RequiredFieldsValidation implements Validation {
+
+  constructor(private readonly fieldName: string) {}
+
+  validate(input: any): Error {
+    if(!input[this.fieldName])
+      return new MissingParamError(this.fieldName)
+  }
+}
