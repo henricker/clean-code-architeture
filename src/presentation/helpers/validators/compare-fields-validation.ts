c158359002546c982766fa63e5a9bc8e11585006
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-22 16:18:27 -0300

    feat: ensure CompareFieldsValidation returns InvalidParamError or null on success

---
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -0,0 +1,10 @@
+import { InvalidParamError } from "../../errors";
+import { Validation } from "../../protocols/validation";
+
+export class CompareFieldsValidator implements Validation {
+  constructor(private readonly field: string, private readonly fieldToCompare: string) {}
+  validate(input: any): Error {
+    if(input[this.field] !== input[this.fieldToCompare])
+      return new InvalidParamError(this.fieldToCompare)
+  }
+}
