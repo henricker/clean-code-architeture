97f8dcfee78c0cad49a01223a491e5b3e6d0c0d2
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-22 16:37:37 -0300

    test: ensure EmailValidation returns InvalidParamError if email is invalid

---
+++ src/presentation/helpers/validators/email-validation.spec.ts
@@ -0,0 +1,41 @@
+import { InvalidParamError } from "../../errors"
+import { EmailValidator } from "../../protocols/email-validator"
+import { EmailValidation } from "./email-validation"
+
+interface ISutType {
+  sut: EmailValidation
+  emailValidatorStub: EmailValidator
+}
+
+const makeEmailValidatorStub = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
+
+const makeSut = (): ISutType => {
+  const emailValidatorStub = makeEmailValidatorStub()
+  const sut = new EmailValidation('email', emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+
+
+describe('EmailValidation', () => {
+
+  it('should return an InvalidParamError if EmailValidator returns false', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const mockObject = { email: 'invalid_email' }
+    const error = sut.validate(mockObject)
+
+    expect(error).toEqual(new InvalidParamError('email'))
+  })
+
+})
