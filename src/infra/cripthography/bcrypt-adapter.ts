994e9830447fa0828424b1fc45eaed74d581f06f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-25 16:24:13 -0300

    feat: ensure BcryptAdapter calls compare method of bcrypt

--- src/infra/cripthography/bcrypt-adapter.ts
+++ src/infra/cripthography/bcrypt-adapter.ts
@@ -1,12 +1,19 @@
 import { Hasher } from "../../data/protocols/criptography/hasher";
 import brcrypt from 'bcrypt'
+import { HashComparer } from "../../data/protocols/criptography/hash-comparer";
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
 
   constructor(private readonly salt: number) {}
-
+  
   async hash(value: string): Promise<string> {
     const hashedValue = await brcrypt.hash(value, this.salt)
     return hashedValue
   }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    await brcrypt.compare(value, hash)
+    return true
+  }
+
 }
