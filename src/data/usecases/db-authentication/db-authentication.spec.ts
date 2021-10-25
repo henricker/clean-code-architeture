63cfc7201028db78f4554aa2ff632658d3d9e8b4
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-25 14:45:36 -0300

    test: ensure DbAuthentication returns null if HashCompare returns false

--- src/data/usecases/db-authentication/db-authentication.spec.ts
+++ src/data/usecases/db-authentication/db-authentication.spec.ts
@@ -91,4 +91,11 @@ describe('DbAuthentication UseCase', () => {
     await expect(promise).rejects.toThrow()
   })
 
+  it('Should return null if HashCompare returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockResolvedValueOnce(false)
+    const accesstoken = await sut.auth(makeFakeAuthentication())
+    expect(accesstoken).toBeNull()
+  })
+
 })
