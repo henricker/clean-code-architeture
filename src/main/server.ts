519c55ac2306cf179a7b991ceceddc16453ba9d3
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-26 09:19:32 -0300

    chore: remove logs on server initializing

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,7 +5,6 @@ MongoHelper
   .connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    console.log(process.env.MONGO_URL)
     app.listen(env.port, () => console.log(`Server running on port: http://localhost:${env.port}`))
   })
   .catch(console.error)
