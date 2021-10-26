507b7004364e53f3b631e26ce62aef4012c998bf
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-26 20:09:21 -0300

    test: ensure AuthMiddleware return 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -58,4 +58,42 @@ describe('Auth Middleware', () => {
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 
+  it('Should return 403 if LoadAccountByToken returns null', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockResolvedValueOnce(null)
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+    // test('Should call LoadAccountByToken with correct accessToken', async () => {
+  //   const role = 'any_role'
+  //   const { sut, loadAccountByTokenSpy } = makeSut(role)
+  //   const httpRequest = mockRequest()
+  //   await sut.handle(httpRequest)
+  //   expect(loadAccountByTokenSpy.accessToken).toBe(httpRequest.accessToken)
+  //   expect(loadAccountByTokenSpy.role).toBe(role)
+  // })
+
+  // test('Should return 403 if LoadAccountByToken returns null', async () => {
+  //   const { sut, loadAccountByTokenSpy } = makeSut()
+  //   loadAccountByTokenSpy.result = null
+  //   const httpResponse = await sut.handle(mockRequest())
+  //   expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  // })
+
+  // test('Should return 200 if LoadAccountByToken returns an account', async () => {
+  //   const { sut, loadAccountByTokenSpy } = makeSut()
+  //   const httpResponse = await sut.handle(mockRequest())
+  //   expect(httpResponse).toEqual(ok({
+  //     accountId: loadAccountByTokenSpy.result.id
+  //   }))
+  // })
+
+  // test('Should return 500 if LoadAccountByToken throws', async () => {
+  //   const { sut, loadAccountByTokenSpy } = makeSut()
+  //   jest.spyOn(loadAccountByTokenSpy, 'load').mockImplementationOnce(throwError)
+  //   const httpResponse = await sut.handle(mockRequest())
+  //   expect(httpResponse).toEqual(serverError(new Error()))
+  // })
+
 })
