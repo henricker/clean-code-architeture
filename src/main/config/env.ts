43f97b03a88e38be4d910c90c2f513d388790113
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-26 09:29:33 -0300

    fix: rename port to PORT env variable

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.port || 5050,
+  port: process.env.PORT || 5050,
   secret: process.env.JWT_SECRET_KEY || 'taoisud12367klfdu123809-12=1e=qe809qkasjdklasjd'
 }
