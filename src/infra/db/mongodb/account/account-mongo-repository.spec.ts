a262ea84279568a0ddb72a299d33c30f1e8cef0a
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-27 09:56:32 -0300

    test: find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -74,5 +74,34 @@ describe('Account Mongo Repository', () => {
     })
   })
 
+  describe('updateAccessToken()', () => {
+    test('Should update the account accessToken on success', async () => {
+      const sut = makeSut()
+      const res = await accountCollection.insertOne(makeFakeAccount())
+      const fakeAccount = res.ops[0]
+      expect(fakeAccount.acessToken).toBeFalsy()
+      await sut.updateAccessToken(fakeAccount._id, 'any_token')
+      const account = await accountCollection.findOne({ _id: fakeAccount._id })
+      expect(account).toBeTruthy()
+      expect(account.accessToken).toBe('any_token')
+    })
+  })
 
+  describe('loadByTokenSuccess()', () => {
+    it('should return an account with token without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@email.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
 })
