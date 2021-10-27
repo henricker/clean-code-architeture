ebd1aa2c03fd4508b6410ade33b9881402e85b3c
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-27 14:59:48 -0300

    feat: ensure SurveyMongoRepository returns all accounts

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,18 @@
 import { AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { LoadSurveysRepository, SurveyModel } from '../../../../data/usecases/load-surveys/db-load-surveys-protocols'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+
   async add(data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne({ ...data })
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = surveyCollection.find().toArray()
+    return surveys
   }
 }
