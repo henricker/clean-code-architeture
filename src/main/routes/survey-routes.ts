1a20cddb604cc79034f191430b5e6699c11c715c
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-27 15:42:12 -0300

    feat: route GET /surveys return 403 if no x-access-token no provided

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { adaptRouter } from '../adapters/express/express-router-adapter'
 import { makeAddSurveyController } from '../factories/controller/survey/add-survey/add-survey-controller-factory'
+import { makeLoadSurveysController } from '../factories/controller/survey/load-survey/load-survey-controller.factory'
+import { auth } from '../middlewares/auth'
 import { authAdmin } from '../middlewares/auth-admin'
 
 export default (router: Router): void => {
   router.post('/surveys', authAdmin, adaptRouter(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRouter(makeLoadSurveysController()))
 }
