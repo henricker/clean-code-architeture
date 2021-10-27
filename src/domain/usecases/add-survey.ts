267e662936ecbc0f09012247edd6553bf75c1ca3
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-27 18:21:23 -0300

    refactor: change duplicated code with Omit utility types of Typescript

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,10 +1,6 @@
-import { SurveyAnswerModel } from '../models/Survey'
+import { SurveyModel } from '../models/Survey'
 
-export type AddSurveyModel = {
-  question: string
-  answers: SurveyAnswerModel[]
-  date: Date
-}
+export type AddSurveyModel = Omit<SurveyModel, "id">
 
 export interface AddSurvey {
   add(data: AddSurveyModel): Promise<void>
