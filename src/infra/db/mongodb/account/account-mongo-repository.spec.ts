0cbcb3dc8fc1dbdcac675661076284341019f258
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-27 09:59:32 -0300

    test: find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -88,6 +88,7 @@ describe('Account Mongo Repository', () => {
   })
 
   describe('loadByTokenSuccess()', () => {
+    
     it('should return an account with token without role', async () => {
       const sut = makeSut()
       await accountCollection.insertOne({
@@ -104,4 +105,22 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
   })
+
+  it('should return an account with token without role', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password',
+      accessToken: 'any_token',
+      role: 'any_role'
+    })
+    const account = await sut.loadByToken('any_token', 'any_role')
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.password).toBe('any_password')
+  })
+  
 })
