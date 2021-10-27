adec5d8140ca97d3919370445e480ce568e90067
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-27 14:59:56 -0300

    test: ensure SurveyMongoRepository returns all accounts

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -38,12 +38,39 @@ describe('Survey Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  it('should return an account on add success', async () => {
-    const sut = makeSut()
-    await sut.add(makeFakeSurveyData())
-
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  describe('add()', () => {
+    it('should return an account on add success', async () => {
+      const sut = makeSut()
+      await sut.add(makeFakeSurveyData())
+  
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
   })
 
+  describe('loadAll()', () => {
+    it('should load all surveys on success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }, {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }]
+      )
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
+  })
 })
