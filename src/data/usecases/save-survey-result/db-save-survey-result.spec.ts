3abc638e68fce61a16613dc49dca4fa8e14f7ebd
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-28 14:32:21 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,52 @@
+import { SaveSurveyResultRepository, SaveSurveyResultModel, SurveyResultModel } from './db-save-survey-result-protocols'
+import { DbSaveSurveyResult } from './db-save-survey-result'
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeSurveyResultModel = (): SurveyResultModel => ({
+  id:'valid_id',
+  accountId: 'valid_account_id',
+  answer: 'valid_answer',
+  date: new Date('2021-10-27T16:16:47.696Z'),
+  surveyId: 'valid_survey_id'
+})
+
+const makeSaveSurveyResultModel = (): SaveSurveyResultModel => ({
+  accountId: 'valid_account_id',
+  answer: 'valid_answer',
+  date: new Date('2021-10-27T16:16:47.696Z'),
+  surveyId: 'valid_survey_id'
+})
+
+
+const makeSaveSurveyResultRepositoryStub = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return makeSurveyResultModel()
+    }
+  }
+
+  return new SaveSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepositoryStub()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+
+  return {
+    sut,
+    saveSurveyResultRepositoryStub
+  }
+}
+
+describe('DbSaveSurveyResult UseCase', () => {
+  it('should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+    await sut.save(makeSaveSurveyResultModel())
+    expect(saveSpy).toHaveBeenCalledWith(makeSaveSurveyResultModel())
+  })
+})
