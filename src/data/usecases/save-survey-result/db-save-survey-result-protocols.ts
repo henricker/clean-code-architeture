75cf04c5f04feb375bb4e22804a5a8ed6597bd66
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-28 14:32:11 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result-protocols.ts
@@ -0,0 +1,4 @@
+export * from '@/domain/models/survey-result'
+export * from '@/domain/usecases/save-survey-result'
+export * from '@/data/protocols/db/survey-result/save-survey-result-repository'
+
