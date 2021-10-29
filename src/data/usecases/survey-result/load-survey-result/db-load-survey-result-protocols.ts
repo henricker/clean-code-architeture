5453583d8b242d5be210c6c2cbf1e759c55da361
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-29 20:51:27 -0300

    feat: ensure DbLoadSurveyResult thorws if LoadSurveyByIdRepository throws

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
@@ -1,4 +1,5 @@
 export * from "@/domain/models/survey-result"
+export * from "@/domain/models/Survey"
 export * from "@/domain/usecases/survey-result/load-survey-result";
 export * from "@/data/usecases/survey/add-survey/db-add-survey";
 export * from "@/data/protocols/db/survey-result/load-survey-result-repository";
--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,7 +1,9 @@
-import { LoadSurveyResultRepository } from "@/data/protocols/db/survey-result/load-survey-result-repository";
-import { LoadSurveyResult } from "@/domain/usecases/survey-result/load-survey-result";
-import { LoadSurveyByIdRepository } from "../../survey/load-survey-by-id/db-load-survey-by-id-protocols";
-import { SurveyResultModel } from "../save-survey-result/db-save-survey-result-protocols";
+import {
+  LoadSurveyResultRepository,
+  LoadSurveyResult,
+  LoadSurveyByIdRepository,
+  SurveyResultModel
+} from './db-load-survey-result-protocols'
 
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
